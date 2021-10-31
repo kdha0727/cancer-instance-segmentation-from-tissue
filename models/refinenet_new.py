d062fc73d798626d678a169117f13ed4bebd14b2
leejunho0421 <55482988+leejunho0421@users.noreply.github.com>
2021-10-31 17:05:22 +0900

    Update refinenet_new.py

--- models/refinenet_new.py
+++ models/refinenet_new.py
@@ -1,143 +1,133 @@
-Skip to content
-DrSleep
-/
-refinenet-pytorch
-Public
-Code
-Issues
-2
-Pull requests
-3
-Actions
-Projects
-Wiki
-More
-refinenet-pytorch/models/resnet.py /
-@DrSleep
-DrSleep First commit
- History
- 1 contributor
-256 lines (205 sloc)  9.29 KB
-   
-"""RefineNet
-RefineNet PyTorch for non-commercial purposes
-Copyright (c) 2018, Vladimir Nekrasov (vladimir.nekrasov@adelaide.edu.au)
-All rights reserved.
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-* Redistributions of source code must retain the above copyright notice, this
-  list of conditions and the following disclaimer.
-* Redistributions in binary form must reproduce the above copyright notice,
-  this list of conditions and the following disclaimer in the documentation
-  and/or other materials provided with the distribution.
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-"""
-
 import torch.nn as nn
 import torch.nn.functional as F
-import torch
 
-import numpy as np
+from typing import *
 
-from utils.helpers import maybe_download
-from utils.layer_factory import conv1x1, conv3x3, CRPBlock, RCUBlock
+from .resnet import BasicBlock, Bottleneck, ResNet, conv3x3, conv1x1
 
-data_info = {
-    21: 'VOC',
-    }
 
-models_urls = {
-    '101_voc'     : 'https://cloudstor.aarnet.edu.au/plus/s/Owmttk9bdPROwc6/download',
-    
-    '101_imagenet': 'https://download.pytorch.org/models/resnet101-5d3b4d8f.pth',
-    }
+class CRPBlock(nn.Sequential):
 
-class BasicBlock(nn.Module):
-    expansion = 1
-
-    def __init__(self, inplanes, planes, stride=1, downsample=None):
-        super(BasicBlock, self).__init__()
-        self.conv1 = conv3x3(inplanes, planes, stride)
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.relu = nn.ReLU(inplace=True)
-        self.conv2 = conv3x3(planes, planes)
-        self.bn2 = nn.BatchNorm2d(planes)
-        self.downsample = downsample
-        self.stride = stride
+    def __init__(self, in_planes, out_planes, n_stages):
+        layers = []
+        for i in range(n_stages):
+            layers.append(nn.Sequential(
+                nn.MaxPool2d(kernel_size=5, stride=1, padding=2),
+                conv3x3(out_planes if i else in_planes, out_planes, stride=1, bias=False)
+            ))
+        super().__init__(*layers)
 
     def forward(self, x):
-        residual = x
-
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-
-        out = self.conv2(out)
-        out = self.bn2(out)
-
-        if self.downsample is not None:
-            residual = self.downsample(x)
-
-        out += residual
-        out = self.relu(out)
-
-        return out
+        y = x
+        for layer in self:
+            x = layer(x)
+            y += x
+        return y
 
 
-class Bottleneck(nn.Module):
-    expansion = 4
+class RCUBlock(nn.Sequential):
 
-    def __init__(self, inplanes, planes, stride=1, downsample=None):
-        super(Bottleneck, self).__init__()
-        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride,
-                               padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(planes)
-        self.conv3 = nn.Conv2d(planes, planes * 4, kernel_size=1, bias=False)
-        self.bn3 = nn.BatchNorm2d(planes * 4)
-        self.relu = nn.ReLU(inplace=True)
-        self.downsample = downsample
-        self.stride = stride
+    def __init__(self, in_planes, out_planes, n_blocks, n_stages):
+        layers = []
+        for i in range(n_blocks):
+            layer = []
+            for j in range(n_stages):
+                layer.append(nn.ReLU())
+                layer.append(conv3x3((in_planes if i or j else out_planes), out_planes, stride=1, bias=(not j)))
+            layers.append(nn.Sequential(*layer))
+        super().__init__(*layers)
 
     def forward(self, x):
-        residual = x
-
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-
-        out = self.conv2(out)
-        out = self.bn2(out)
-        out = self.relu(out)
-
-        out = self.conv3(out)
-        out = self.bn3(out)
-
-        if self.downsample is not None:
-            residual = self.downsample(x)
-
-        out += residual
-        out = self.relu(out)
-
-        return out
+        for layer in self:
+            residual = x
+            x = layer(x)
+            x += residual
+        return x
 
 
-class RefineNet(nn.Module):
+class RefineBlock(nn.Module):
 
-    def __init__(self, block, layers, num_classes=21):
-        self.inplanes = 64
-        super(RefineNet, self).__init__()
-        self.do = nn.Dropout(p=0.5)
+    def __init__(
+            self,
+            in_channels: int,
+            mid_channels: int,
+            out_channels: int = None,
+            top: bool = False,
+            bottom: bool = False,
+            light_weight: bool = False,
+    ) -> None:
+
+        assert not (top and bottom)
+        assert bottom or out_channels
+
+        super().__init__()
+        self.top = top
+        self.bottom = bottom
+        self.light_weight = light_weight
+        conv = conv1x1 if light_weight else conv3x3
+
+        self.p_ims1d2_out_dimred = conv(in_channels, mid_channels, bias=False)
+        if not light_weight:
+            self.adapt_stage_b = RCUBlock(mid_channels, mid_channels, 2, 2)  # light weight: omit
+        if not top:
+            self.adapt_stage_b2_joint_varout_dimred = conv(256, 256, bias=False)
+        self.mflow_conv_g_pool = CRPBlock(mid_channels, mid_channels, 4)
+        if not light_weight:
+            self.mflow_conv_g_b = RCUBlock(mid_channels, mid_channels, 3, 2)  # light weight: omit
+        if not bottom:
+            self.mflow_conv_g_b3_joint_varout_dimred = conv(mid_channels, out_channels, bias=False)
+
+    def forward(self, x, prior=None):
+
+        top = self.top
+        bottom = self.bottom
+        light_weight = self.light_weight
+        assert (prior is not None) ^ top
+
+        x = self.p_ims1d2_out_dimred(x)
+        if not light_weight:
+            x = self.adapt_stage_b(x)
+        if not top:
+            x = self.adapt_stage_b2_joint_varout_dimred(x)
+            x = x + F.interpolate(prior, size=x.size()[-2:], mode="bilinear", align_corners=True)
+        x = F.relu(x)
+        x = self.mflow_conv_g_pool(x)
+        if not light_weight:
+            x = self.mflow_conv_g_b(x)
+        if not bottom:
+            x = self.mflow_conv_g_b3_joint_varout_dimred(x)
+        return x
+
+
+class RefineNet(ResNet):
+
+    def __init__(
+            self,
+            block: Type[Union[BasicBlock, Bottleneck]],
+            layers: List[int],
+            n_classes: int = 1000,
+            n_channels: int = 3,
+            groups: int = 1,
+            width_per_group: int = 64,
+            light_weight: bool = False,
+            in_planes: int = 64,
+            dropout: float = 0.5,
+            min_refine_planes: int = 256,
+            replace_stride_with_dilation: Optional[List[bool]] = None,
+            init_weight: bool = True,
+    ) -> None:
+
+        super().__init__(
+            block, layers,
+            include_top=False, n_channels=n_channels, groups=groups, width_per_group=width_per_group,
+            in_planes=in_planes, replace_stride_with_dilation=replace_stride_with_dilation, init_weight=False
+        )
+
+        self.do = nn.Dropout(dropout)
+
+        out_channels = in_planes
+        refine_block = []
+
         self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3,
                                bias=False)
         self.bn1 = nn.BatchNorm2d(64)
@@ -200,7 +190,7 @@ class RefineNet(nn.Module):
 
         return nn.Sequential(*layers)
 
-    def forward(self, x):
+def forward(self, x):
         x = self.conv1(x)
         x = self.bn1(x)
         x = self.relu(x)
@@ -255,30 +245,25 @@ class RefineNet(nn.Module):
         return out
 
 
-def rf101(num_classes, imagenet=False, pretrained=True, **kwargs):
-    model = RefineNet(Bottleneck, [3, 4, 23, 3], num_classes=num_classes, **kwargs)
-    if imagenet:
-        key = '101_imagenet'
-        url = models_urls[key]
-        model.load_state_dict(maybe_download(key, url), strict=False)
-    elif pretrained:
-        dataset = data_info.get(num_classes, None)
-        if dataset:
-            bname = '101_' + dataset.lower()
-            key = 'rf' + bname
-            url = models_urls[bname]
-            model.load_state_dict(maybe_download(key, url), strict=False)
-    return model
-© 2021 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
-Loading complete
+def refinenet50(n_classes, **kwargs):
+    return RefineNet(Bottleneck, [3, 4, 6, 3], n_classes=n_classes, **kwargs)
+
+
+def refinenet101(n_classes, **kwargs):
+    return RefineNet(Bottleneck, [3, 4, 23, 3], n_classes=n_classes, **kwargs)
+
+
+def refinenet152(n_classes, **kwargs):
+    return RefineNet(Bottleneck, [3, 8, 36, 3], n_classes=n_classes, **kwargs)
+
+
+def rf_lw50(n_classes, **kwargs):
+    return RefineNet(Bottleneck, [3, 4, 6, 3], n_classes=n_classes, light_weight=True, **kwargs)
+
+
+def rf_lw101(n_classes, **kwargs):
+    return RefineNet(Bottleneck, [3, 4, 23, 3], n_classes=n_classes, light_weight=True, **kwargs)
+
+
+def rf_lw152(n_classes, **kwargs):
+    return RefineNet(Bottleneck, [3, 8, 36, 3], n_classes=n_classes, light_weight=True, **kwargs)
